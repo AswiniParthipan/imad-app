51491f31864128af04e4e64bf088dd56b5fd6616
AswiniParthipan <33031218+AswiniParthipan@users.noreply.github.com>
2017-10-28 18:46:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.get('/article-one', function(req , res){
      res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 
 app.get('/article-two', function(req , res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    
+     res.send('articale Two requested and willbe served here');
 });
 });
 
