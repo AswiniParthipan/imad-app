35b9f2fa0fa502c0e17295f105c62df66b917a6b
AswiniParthipan <33031218+AswiniParthipan@users.noreply.github.com>
2017-10-28 18:47:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/article-one', function(req , res){
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
      res.send('articale Two requested and willbe served here');
 
 app.get('/article-two', function(req , res){
